21e72f9e775ed5887af036334af1995e31f50d8b
luisruisinger <luisruisinger.uni@gmail.com>
2025-02-17 19:33:50 +0100

    [Feature]
    * sequential radix sort
    * parallel radix sort is broken and needs to be rewritten into a type dependant macro

--- util/radix_sort.c
+++ util/radix_sort.c
@@ -1,89 +1,80 @@
+#include <assert.h>
+
 #include "radix_sort.h"
 #include "datastructures/bitmap.h"
 #include "threading/thread.h"
 #include "threading/threadpool.h"
-
-#define BITS_PER_ITERATION 8 
+#include "fmt.h"
 
 typedef struct Radix_Args_t {
-    u8 *src;
-    u8 *cpy;
-
-    u32 shift;
-    u32 len;
-    u32 *cnt;
-    u32 *idx;
+    u64 *src;
+    u64 *cpy;
+    
+    usize *cnt;
+    usize *idx;
+    usize len;
+    usize shift;
 } Radix_Args;
 
-#define RADIX_MASK \
-    ((1ULL << BYTE_SIZE) - 1)
-
-#define RADIX_BUFFER_SIZE \
-    (1ULL << BYTE_SIZE)
-
-#define RND_POW_2(_n, _p) \
-    ((_n) + (_p) - 1) & ~((_p) - 1)
-
-#define TRESHOLD 1024
-
-void *sort_range(void *args) {
+void bucket(void *args) {
     Radix_Args *r_args = args;
-    memset(r_args->cnt, 0, r_args->cnt);
 
     for (usize i = 0; i < r_args->len; ++i) {
-        u32 bucket = (r_args->src[i] >> r_args->shift) & RADIX_MASK; 
+        u64 bucket = (r_args->src[i] >> r_args->shift) & RADIX_MASK;
+        u64 _index = r_args->idx[bucket]++;
+        r_args->cpy[_index] = r_args->src[i];
+    }
+}
+
+void sort_range(void *args) {
+    Radix_Args *r_args = args;
+    memset(r_args->cnt, 0, RADIX_BUFFER_SIZE * sizeof(usize));
+
+    for (usize i = 0; i < r_args->len; ++i) {
+        u64 bucket = (r_args->src[i] >> r_args->shift) & RADIX_MASK; 
         ++r_args->cnt[bucket];
     }
 
     usize next_index = 0;
-    for (usize i = 0; i < r_args->cnt; ++i) {
+    for (usize i = 0; i < RADIX_BUFFER_SIZE; ++i) {
         r_args->idx[i] = next_index;
         next_index += r_args->cnt[i];
     }
 
-    for (usize i = 0; i < r_args->len; ++i) {
-        u32 bucket = (r_args->src[i] >> r_args->shift) & RADIX_MASK;
-        u32 _index = r_args->idx[bucket]++;
-        r_args->cpy[_index] = r_args->src[i];
-    }
-
-    return NULL;
+    bucket(args);
 }
 
-void radix_sort(u8 *__restrict keys, u8 *__restrict keys_cpy, usize bit_cnt, usize len);
+/* TODO */
+// fix
+// make into type dependant macro
+void radix_sort_parallel(u64 *__restrict src, u64 *__restrict cpy, usize len) { 
+    usize thread_cnt = 1;
+    usize upper_bound_addr = (usize) (src + len);
+    usize batch = RND_POW_2(len, thread_cnt) / thread_cnt;
 
-void radix_sort_parallel(u8 *__restrict src, u8 *__restrict cpy, usize bit_cnt, usize len) {
-    if (len < TRESHOLD) {
-        radix_sort(src, cpy, bit_cnt, len);
-        return;
+    // setup stage
+    usize *cnt = malloc(RADIX_BUFFER_SIZE * thread_cnt * sizeof(usize));
+    usize *idx = malloc(RADIX_BUFFER_SIZE * thread_cnt * sizeof(usize));
+    Radix_Args args[thread_cnt];
+
+    for (usize j = 0; j < thread_cnt; ++j) {
+        usize rem = (upper_bound_addr - (usize) &src[j * batch]) / sizeof(usize);
+            
+        args[j].len = MIN(rem, batch);
+        args[j].cnt = &cnt[j * RADIX_BUFFER_SIZE];
+        args[j].idx = &idx[j * RADIX_BUFFER_SIZE];
     }
 
-    usize byte_cnt = bit_cnt / BYTE_SIZE;   
-    usize thread_cnt = MAX(2, hardware_concurrency());
+    // sort stage
     Threadpool *tp = threadpool_create(thread_cnt);
 
-    usize args_size = RND_POW_2(thread_cnt * sizeof(Radix_Args), byte_cnt);
-    usize cnt_size  = RADIX_BUFFER_SIZE * thread_cnt * byte_cnt;
-    usize idx_size  = RADIX_BUFFER_SIZE * thread_cnt * byte_cnt;
-
-    u8 *mem = malloc(args_size + cnt_size + idx_size);
-
-    usize upper_bound_addr = (usize) src + (byte_cnt * len);
-    usize batch = RND_POW_2(len, thread_cnt);
-    for (usize i = 0; i < byte_cnt; ++i) {
+    for (usize i = 0; i < sizeof(u64); ++i) {
         for (usize j = 0; j < thread_cnt; ++j) {
-            Radix_Args *args = (Radix_Args *) mem + j;
-            args->len = MIN(upper_bound_addr - (usize) args->src, batch);
-            args->src = (u8 *) src + (byte_cnt * batch * j);
-            args->cpy = (u8 *) cpy + (byte_cnt * batch * j);
+            args[j].src = &src[j * batch];
+            args[j].cpy = &cpy[j * batch];
+            args[j].shift = i * BYTE_SIZE;
 
-            usize base_off_cnt = args_size;
-            usize base_off_idx = args_size + cnt_size;
-            args->cnt = (u8 *) mem + base_off_cnt + (batch * j);
-            args->idx = (u8 *) mem + base_off_idx + (batch * j);
-
-            args->shift = i * BYTE_SIZE;
-            threadpool_enqueue(tp, sort_range, args);
+            threadpool_enqueue(tp, sort_range, &args[j]);
         }
 
         threadpool_wait(tp);
@@ -92,36 +83,23 @@ void radix_sort_parallel(u8 *__restrict src, u8 *__restrict cpy, usize bit_cnt, 
         usize next_idx = 0;
         for (usize j = 0; j < RADIX_BUFFER_SIZE; ++j) {
             for (usize k = 0; k < thread_cnt; ++k) {
-                Radix_Args *args = (Radix_Args *) mem + k;
-
-                args->idx[j] = next_idx;
-                next_idx += args->cnt[j];
+                args[k].idx[j] = next_idx;
+                next_idx += args[k].cnt[j];
             }
         }
 
         for (usize j = 0; j < thread_cnt; ++j) {
-            Radix_Args *args = (Radix_Args *) mem + j;
-            args->len = MIN(upper_bound_addr - (usize) args->src, batch);
-            
-            // swap without temporary 
-            // kinda useless with modern compilers
-            args->src = (u8 *) ((usize) args->src ^ (usize) args->cpy);
-            args->cpy = (u8 *) ((usize) args->src ^ (usize) args->cpy);
-            args->src = (u8 *) ((usize) args->src ^ (usize) args->cpy);
+            args[j].src = args[j].cpy;
+            args[j].cpy = src;
 
-            usize base_off_cnt = args_size;
-            usize base_off_idx = args_size + cnt_size;
-            args->cnt = (u8 *) mem + base_off_cnt + (batch * j);
-            args->idx = (u8 *) mem + base_off_idx + (batch * j);
-
-            args->shift = i * BYTE_SIZE;
-            threadpool_enqueue(tp, sort_range, args);
+            threadpool_enqueue(tp, bucket, &args[j]);
         }
 
         threadpool_wait(tp);
         threadpool_clear_work(tp);
     }
 
-    free(mem);
+    free(cnt);
+    free(idx);
     threadpool_destroy(tp);
 }
--- util/radix_sort.h
+++ util/radix_sort.h
@@ -1,11 +1,58 @@
 #ifndef CPU_RAYTRACING_RADIX_SORT_H
 #define CPU_RAYTRACING_RADIX_SORT_H
 
+#include <string.h>
+
 #include "defines.h"
 
-void radix_sort_parallel(
-    u8 *__restrict keys, u8 *__restrict keys_cpy,
-    u8 *__restrict values, u8 *__restrict values_cpy, 
-    usize len, usize bit_cnt);
+#define RADIX_MASK \
+    ((1ULL << BYTE_SIZE) - 1)
+
+#define RADIX_BUFFER_SIZE \
+    (1ULL << BYTE_SIZE)
+
+#define RADIX_SORT_IMPL(_type) \
+    static void radix_sort_##_type(_type *__restrict src, _type *__restrict cpy, usize len) {       \
+        _type *__restrict _src = src;                                                               \
+        _type *__restrict _cpy = cpy;                                                               \
+                                                                                                    \
+        usize idx[RADIX_BUFFER_SIZE];                                                               \
+        for (usize j = 0; j < sizeof(_type); ++j) {                                                 \
+            memset(idx, 0, sizeof(idx));                                                            \
+                                                                                                    \
+            usize shift = BYTE_SIZE * j;                                                            \
+            usize next_index = 0;                                                                   \
+            usize cnt = 0;                                                                          \
+                                                                                                    \
+            for (usize i = 0; i < len; ++i) {                                                       \
+                ++idx[(_src[i] >> shift) & RADIX_MASK];                                             \
+            }                                                                                       \
+                                                                                                    \
+            for (usize i = 0; i < RADIX_BUFFER_SIZE; ++i){                                          \
+                cnt = idx[i];                                                                       \
+                idx[i] = next_index;                                                                \
+                next_index += cnt;                                                                  \
+            }                                                                                       \
+                                                                                                    \
+            for (usize i = 0; i < len; ++i) {                                                       \
+                usize _idx = idx[(_src[i] >> shift) & RADIX_MASK];                                  \
+                _cpy[_idx] = _src[i];                                                               \  
+                                                                                                    \
+                ++idx[(_src[i] >> shift) & RADIX_MASK];                                             \                                                               
+            }                                                                                       \
+                                                                                                    \
+            _src = (_type *) ((usize) _src ^ (usize) _cpy);                                         \
+            _cpy = (_type *) ((usize) _src ^ (usize) _cpy);                                         \
+            _src = (_type *) ((usize) _src ^ (usize) _cpy);                                         \
+        }                                                                                           \
+    }
+
+RADIX_SORT_IMPL(u64)
+
+#define RADIX_SORT(_type, ...) \
+    radix_sort_##_type(__VA_ARGS__)
+
+
+void radix_sort_parallel(u64 *__restrict src, u64 *__restrict cpy, usize len);
 
 #endif //CPU_RAYTRACING_RADIX_SORT_H
